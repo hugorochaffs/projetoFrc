6cdb8af1e503862278b4231b16cc648050e609b6
hugorochaffs <54285732+hugorochaffs@users.noreply.github.com>
2023-12-16 05:05:35 -0300

    criando servidor

---
+++ servidorDialogo/mysite/mysite/asgi.py
@@ -0,0 +1,31 @@
+"""
+ASGI config for mysite project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import chat.routing
+import os
+
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    # Just HTTP for now. (We can add other protocols later.)
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    )
+})
