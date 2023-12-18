de125dd9834e855b148d2eee3e1159f877d8f38f
hugorochaffs <54285732+hugorochaffs@users.noreply.github.com>
2023-12-18 12:02:55 -0300

    Implementando os status

---
+++ status/js/main.js
@@ -0,0 +1,184 @@
+
+username = "ABC"+ Math.random();
+
+
+
+
+function pessoas(nickname,ttl){
+    this.nickname = nickname;
+    this.ttl = ttl;
+}
+
+const guardaPessoas = [];
+
+function connectWebSocket(){
+
+    console.log("my id:"+ username);
+
+    var loc = window.location;
+    var wsStart = 'ws://';
+
+    if(loc.protocol == 'https:'){
+        wsStart = 'wss://';
+    }
+
+    //var endPoint = wsStart + loc.host + loc.pathname;
+    var endPoint = 'ws://localhost:8765/communications/c1';
+
+    console.log(endPoint);
+
+    webSocket = new WebSocket(endPoint);
+
+    webSocket.addEventListener('open', (e)=>{
+        console.log('Connection Opened!');
+
+        //sendSignal('new-peer', {});
+    });
+    webSocket.addEventListener('message', webSocketOnMessage);
+    webSocket.addEventListener('close',(e)=>{
+        console.log('Connection Closed!');
+    });
+    webSocket.addEventListener('error',(e)=>{
+        console.log('Error!');
+    });
+
+}
+
+function connectWebSocketOnlineStatus(){
+
+    
+
+    console.log("Enviando Status");
+
+    var loc = window.location;
+    var wsStart = 'ws://';
+
+    if(loc.protocol == 'https:'){
+        wsStart = 'wss://';
+    }
+
+    //var endPoint = wsStart + loc.host + loc.pathname;
+    var endPoint = 'ws://localhost:8765/status';
+
+
+    webSocket = new WebSocket(endPoint);
+
+    webSocket.addEventListener('open', (e)=>{
+        console.log('Connection Opened!');
+
+        //sendSignal('new-peer', {});
+    });
+    webSocket.addEventListener('message', atualizaStatus);
+    webSocket.addEventListener('close',(e)=>{
+        console.log('Connection Closed!');
+    });
+    webSocket.addEventListener('error',(e)=>{
+        console.log('Error!');
+    });
+
+}
+
+connectWebSocket();
+connectWebSocketOnlineStatus();
+
+function webSocketOnMessage(event){
+    var parsedData = JSON.parse(event.data);
+    var peerUsername = parsedData['peer'];
+    var action = parsedData['action'];
+    var msg = parsedData['message'];
+
+    if(peerUsername == username){
+        alert("mensagem recebida: "+ msg);
+    }
+
+
+    console.log('message:', parsedData);
+}
+
+function atualizaStatus(event){
+    var parsedData = JSON.parse(event.data);
+    var peerUsername = parsedData['peer'];
+
+    var p = guardaPessoas.find(person => person.nickname === peerUsername);
+
+    if (!p){
+
+        var p = new pessoas(peerUsername, '5');
+
+        guardaPessoas.push(p);
+    }
+    else{
+        p.ttl = 5;
+    }
+    mostraOnline();
+}
+
+function enviaOnline(){
+
+    var jsonStr = JSON.stringify({
+        'peer': username,
+        
+    });
+    webSocket.send(jsonStr);}
+
+
+
+
+    function atualizaPessoasOnline(){
+
+    guardaPessoas.forEach(pessoas => {
+        pessoas.ttl--;
+
+        if(pessoas.ttl<1)
+            guardaPessoas.pop(pessoas);
+    });
+
+
+    
+}
+
+function mostraOnline(){
+
+    var onlineList = document.querySelector('#online-list');
+
+    onlineList.innerHTML = '';
+
+    guardaPessoas.forEach(pessoas => {
+        var li = document.createElement('li');
+        li.appendChild(document.createTextNode(pessoas.nickname));
+        onlineList.appendChild(li);
+    });
+}
+
+setInterval(enviaOnline, 500);
+setInterval(atualizaPessoasOnline, 1000);
+
+var messageList = document.querySelector('#message-list');
+var messageInput = document.querySelector('#msg');
+var dest = document.querySelector('#dest');
+var btnMessage = document.querySelector('#btn-send-msg');
+btnMessage.addEventListener('click', sendMsgOnClick);
+
+    
+
+    function sendMsgOnClick(){
+
+
+        var message = messageInput.value;
+
+        var li = document.createElement('li');
+        li.appendChild(document.createTextNode('Me: ' + message));
+        messageList.appendChild(li);
+
+            var jsonStr = JSON.stringify({
+                   'peer': dest.value,
+                   'action': "teste",
+                   'message': message,
+               });
+               webSocket.send(jsonStr);
+        
+
+        messageInput.value = '';
+
+    }
+    
--- status/js/main_commented.js
+++ status/js/main_commented.js
@@ -1,69 +1,77 @@
+// Exibe uma mensagem no console indicando que o script main.js foi iniciado.
 console.log("in main.js");
+// Objeto que mapeia os pares (peers).
 var mapPeers = {};
 
-
+// Seleciona elementos do DOM.
 var usernameInput = document.querySelector('#username');
 var btnJoin = document.querySelector('#btn-join');
 
+// Variável para armazenar o nome de usuário.
 var username;
 
+// Objeto WebSocket.
 var webSocket;
 
+// Função chamada quando ocorre um evento 'message' no WebSocket.
 function webSocketOnMessage(event){
+    // Analisa os dados recebidos como JSON.
     var parsedData = JSON.parse(event.data);
     var peerUsername = parsedData['peer'];
     var action = parsedData['action'];
 
+    // Verifica se a mensagem pertence ao próprio usuário.
     if(username == peerUsername){
         return;
     }
 
+    // Obtém o nome do canal de recepção.
     var receiver_channel_name = parsedData['message']['receiver_channel_name'];
 
+    // Realiza ações com base na mensagem recebida.
     if(action == 'new-peer'){
         createOfferer(peerUsername, receiver_channel_name);
-
         return;
     }
 
     if(action == 'new-offer'){
         var offer = parsedData['message']['sdp'];
-
         createAnswerer(offer, peerUsername, receiver_channel_name);
-
         return;
     }
 
     if(action == 'new-answer'){
         var answer = parsedData['message']['sdp'];
-
         var peer = mapPeers[peerUsername][0];
-
         peer.setRemoteDescription(answer);
-
         return;
     }
 
     console.log('message:', message);
 }
 
+// Adiciona um ouvinte de evento 'click' ao botão de entrada.
 btnJoin.addEventListener('click',()=>{
+    // Obtém o nome de usuário inserido pelo usuário.
     username = usernameInput.value;
 
+    // Verifica se o nome de usuário não está vazio.
     if(username == ''){
         return;
     }
 
+    // Realiza algumas operações na interface do usuário após a entrada do usuário.
     usernameInput.value = '';
     usernameInput.disabled = true;
     usernameInput.style.visibility = 'hidden';
-
     btnJoin.disabled = true;
     btnJoin.style.visibility = 'hidden';
 
+    // Atualiza o elemento de exibição do nome de usuário.
     var labelUsername = document.querySelector('#label-username');
     labelUsername.innerHTML = username;
 
+    // Obtém o protocolo e o endpoint para o WebSocket.
     var loc = window.location;
     var wsStart = 'ws://';
 
@@ -71,16 +79,15 @@ btnJoin.addEventListener('click',()=>{
         wsStart = 'wss://';
     }
 
-    //var endPoint = wsStart + loc.host + loc.pathname;
-    var endPoint = 'ws://localhost:8765/communications/c1';
+    var endPoint = wsStart + loc.host + loc.pathname;
 
+    // Exibe o endpoint no console.
     console.log(endPoint);
 
+    // Inicia o WebSocket e adiciona ouvintes de eventos.
     webSocket = new WebSocket(endPoint);
-
     webSocket.addEventListener('open', (e)=>{
         console.log('Connection Opened!');
-
         sendSignal('new-peer', {});
     });
     webSocket.addEventListener('message', webSocketOnMessage);
@@ -92,132 +99,86 @@ btnJoin.addEventListener('click',()=>{
     });
 });
 
+// Inicializa uma instância de MediaStream para a câmera e o microfone.
 var localStream = new MediaStream();
-
 const constraints = {
     'video': true,
     'audio': true
 };
 
+// Obtém a referência aos elementos de vídeo e botões.
 const localVideo  = document.querySelector('#local-video');
-
 const btnToggleAudio  = document.querySelector('#btn-toggle-audio');
-
 const btnToggleVideo  = document.querySelector('#btn-toggle-video');
 
+// ...
 
+// Obtém referências a elementos da interface relacionados a mensagens.
+var messageList = document.querySelector('#message-list');
+var messageInput = document.querySelector('#msg');
+var btnMessage = document.querySelector('#btn-send-msg');
 
+// Adiciona ouvinte de evento ao botão de envio de mensagem.
+btnMessage.addEventListener('click', sendMsgOnClick);
 
-var userMedia = navigator.mediaDevices.getUserMedia(constraints)
-    .then(stream =>{
-        localStream = stream;
-        localVideo.srcObject = localStream;
-        localVideo.muted = true;
+// Função para enviar mensagem quando o botão de envio é clicado.
+function sendMsgOnClick(){
+    var message = messageInput.value;
 
-        var audioTracks = stream.getAudioTracks();
-        var videoTracks = stream.getVideoTracks();
+    // Adiciona a mensagem à lista de mensagens localmente.
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode('Me: ' + message));
+    messageList.appendChild(li);
 
-        audioTracks[0].enabled = true;
-        videoTracks[0].enabled = true;
+    // Obtém os canais de dados e envia a mensagem para cada um.
+    var dataChannels = getDataChannels();
+    message = username + ': ' + message;
 
-        btnToggleAudio.addEventListener('click', ()=>{
-            audioTracks[0].enabled = !audioTracks[0].enabled;
+    for(index in dataChannels){
+        dataChannels[index].send(message);
+        console.log('loop');
+    }
 
-            if(audioTracks[0].enabled){
-                btnToggleAudio.innerHTML = 'mute';
-                return;
-            }
-            btnToggleAudio.innerHTML = 'unmute';
+    // Limpa o campo de entrada de mensagem.
+    messageInput.value = '';
+}
 
-
-
-        });
-
-        btnToggleVideo.addEventListener('click', ()=>{
-            videoTracks[0].enabled = !videoTracks[0].enabled;
-
-            if(videoTracks[0].enabled){
-                btnToggleVideo.innerHTML = 'Turn video off';
-                return;
-            }
-            btnToggleVideo.innerHTML = 'Turn video on';
-
-
-
-        });
-    })
-    .catch(error =>{
-        console.log('Error acessing media', error);
+// Função para enviar sinalização através do WebSocket.
+function sendSignal(action, message){
+    var jsonStr = JSON.stringify({
+        'peer': username,
+        'action': action,
+        'message': message,
     });
+    webSocket.send(jsonStr);
+}
 
-  
-
-    var messageList = document.querySelector('#message-list');
-    var messageInput = document.querySelector('#msg');
-    var btnMessage = document.querySelector('#btn-send-msg');
-
-    btnMessage.addEventListener('click', sendMsgOnClick);
-
-    
-
-    function sendMsgOnClick(){
-
-
-        var message = messageInput.value;
-
-        var li = document.createElement('li');
-        li.appendChild(document.createTextNode('Me: ' + message));
-        messageList.appendChild(li);
-
-        var dataChannels = getDataChannels();
-
-        message = username + ': ' + message;
-
-        for(index in dataChannels){
-
-            dataChannels[index].send(message);
-            console.log('loop');
-
-        }
-
-        messageInput.value = '';
-
-    }
-
-   
-
-    
-
-    function sendSignal(action, message){
-
-     var jsonStr = JSON.stringify({
-            'peer': username,
-            'action': action,
-            'message': message,
-        });
-        webSocket.send(jsonStr);
-    }
-
+// Função para criar um ofertante (peer) WebRTC.
 function createOfferer(peerUsername, receiver_channel_name){
+    // Cria uma nova instância de RTCPeerConnection.
     var peer = new RTCPeerConnection(null);
 
+    // Adiciona as faixas locais ao peer.
     addLocalTracks(peer);
 
+    // Cria um canal de dados.
     var dc  = peer.createDataChannel('channel');
     dc.addEventListener('open', ()=>{
         console.log('Connection Openned!');
-
     });
     
     dc.addEventListener('message', dcOnMessage);
 
+    // Cria um elemento de vídeo remoto.
     var remoteVideo = createVideo(peerUsername);
     setOnTrack(peer, remoteVideo);
 
+    // Mapeia o peer e o canal de dados.
     mapPeers[peerUsername] = [peer, dc];
 
+    // Adiciona ouvinte para alterações no estado da conexão ICE.
     peer.addEventListener('iceconnectionstatechange', ()=>{
-        var iceconnectionstatechange = peer.iceConnectionState;
+        var iceConnectionState = peer.iceConnectionState;
 
         if(iceConnectionState === 'failed'|| iceConnectionState === 'disconnected'|| iceConnectionState === 'closed'){
             delete mapPeers[peerUsername];
@@ -227,59 +188,57 @@ function createOfferer(peerUsername, receiver_channel_name){
             }
             removeVideo(remoteVideo);
         }
-
-
     });
 
+    // Adiciona ouvinte para candidatos ICE.
     peer.addEventListener('icecandidate',(event)=>{
         if(event.candidate){
             console.log('new ice candidate:', JSON.stringify(peer.localDescription));
-
             return;
         }
         
+        // Envia sinalização 'new-offer'.
         sendSignal('new-offer', {
             'sdp': peer.localDescription,
             'receiver_channel_name': receiver_channel_name
         });
     });
 
+    // Cria uma oferta e define a descrição local.
     peer.createOffer()
         .then(o => peer.setLocalDescription(o))
         .then(()=>{
             console.log('Local description set successfully.');
         });
-
 }
 
+// Função para criar um respondente (peer) WebRTC.
 function createAnswerer(offer, peerUsername, receiver_channel_name){
-
+    // Cria uma nova instância de RTCPeerConnection.
     var peer = new RTCPeerConnection(null);
 
+    // Adiciona as faixas locais ao peer.
     addLocalTracks(peer);
 
-    
-
+    // Cria um elemento de vídeo remoto.
     var remoteVideo = createVideo(peerUsername);
     setOnTrack(peer, remoteVideo);
 
+    // Adiciona ouvinte para eventos de canal de dados.
     peer.addEventListener('datachannel', e =>{
         peer.dc = e.channel;
 
         peer.dc.addEventListener('open', ()=>{
             console.log('Connection Openned!');
-    
         });
         
         peer.dc.addEventListener('message', dcOnMessage);
         mapPeers[peerUsername] = [peer, peer.dc];
-
     });
 
-    
-
+    // Adiciona ouvinte para alterações no estado da conexão ICE.
     peer.addEventListener('iceconnectionstatechange', ()=>{
-        var iceconnectionstatechange = peer.iceConnectionState;
+        var iceConnectionState = peer.iceConnectionState;
 
         if(iceConnectionState === 'failed'|| iceConnectionState === 'disconnected'|| iceConnectionState === 'closed'){
             delete mapPeers[peerUsername];
@@ -289,39 +248,39 @@ function createAnswerer(offer, peerUsername, receiver_channel_name){
             }
             removeVideo(remoteVideo);
         }
-
-
     });
 
+    // Adiciona ouvinte para candidatos ICE.
     peer.addEventListener('icecandidate',(event)=>{
         if(event.candidate){
             console.log('new ice candidate:', JSON.stringify(peer.localDescription));
-
             return;
         }
         
+        // Envia sinalização 'new-answer'.
         sendSignal('new-answer', {
             'sdp': peer.localDescription,
             'receiver_channel_name': receiver_channel_name
         });
     });
 
+    // Define a descrição remota com a oferta recebida.
     peer.setRemoteDescription(offer)
         .then(()=> {
-            console.log('Remote description set sucessfully for %s.', peerUsername);
+            console.log('Remote description set successfully for %s.', peerUsername);
 
+            // Cria uma resposta.
             return peer.createAnswer();
         })
         .then(a => {
             console.log('Answer created!');
 
+            // Define a descrição local com a resposta.
             peer.setLocalDescription(a);
-        })
-
+        });
 }
 
-
-
+// Função para adicionar faixas locais ao peer.
 function addLocalTracks(peer){
     localStream.getTracks().forEach(track =>{
         peer.addTrack(track, localStream);
@@ -329,16 +288,17 @@ function addLocalTracks(peer){
     return;
 }
 
-
+// Função chamada quando ocorre um evento de mensagem no canal de dados.
 function dcOnMessage(event){
     var message = event.data;
 
+    // Adiciona a mensagem à lista de mensagens.
     var li = document.createElement('li');
     li.appendChild(document.createTextNode(message));
     messageList.appendChild(li);
-
 }
 
+// Função para criar um elemento de vídeo remoto.
 function createVideo(peerUsername){
     var videoContainer = document.querySelector('#video-container');
     var remoteVideo = document.createElement('video');
@@ -353,9 +313,9 @@ function createVideo(peerUsername){
     videoWrapper.appendChild(remoteVideo);
 
     return remoteVideo;
-
 }
 
+// Função para configurar o ouvinte de faixa no peer.
 function setOnTrack(peer, remoteVideo){
     var remoteStream = new MediaStream();
 
@@ -363,15 +323,17 @@ function setOnTrack(peer, remoteVideo){
 
     peer.addEventListener('track', async (event) =>{
         remoteStream.addTrack(event.track, remoteStream);
-    })
+    });
 }
+
+// Função para remover o elemento de vídeo.
 function removeVideo(video){
     var videoWrapper = video.parentNode;
 
     videoWrapper.parentNode.removeChild(videoWrapper);
-
 }
 
+// Função para obter os canais de dados de todos os pares mapeados.
 function getDataChannels(){
     var dataChannels = [];
 
